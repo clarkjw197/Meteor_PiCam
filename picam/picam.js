0ba74065a6eba27e3ec3eabaabf304cbdd6b8813
John Clark <clarkjw197@potsdam.edu>
2016-02-09 19:09:22 -0500

    Initial commit.  Currently have basic site with webcam runningproperly.

---
+++ picam/picam.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+    photo: function () {
+      return Session.get("photo");
+    },
+
+  });
+
+  Template.body.events({
+    'click button': function () {
+      var cameraOptions = {
+        width: 800,
+        height: 600
+      };
+
+      MeteorCamera.getPicture(cameraOptions, function (error, data) {
+        Session.set("photo", data);
+      });
+    }
+  });
+}
+
+  
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+
+  });
+}
+
